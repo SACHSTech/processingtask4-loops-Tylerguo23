b067668fccde07520613269096622d326d071939
jiayetguo <jiayeguo@gmail.com>
2022-03-24 22:35:29 -0400

    Finished grid and circles

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -16,20 +16,33 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
+    midWidth = width / 2;
+    midHeight = height / 2;
   }
+  float midWidth;
+  float midHeight;
+
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    stroke(0);
+    strokeWeight(5);
+	  for (int x = width / 20; x < midWidth; x += width / 20){
+        line(x, 0, x, midHeight);
+    }
+    for (int y = height / 20; y < midHeight; y += height / 20){
+      line(0, y, midWidth, y);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(220, 160, 160);
+    for (int circleX = (int)(midWidth + width / 12); circleX <= width - width / 12; circleX += width / 12){
+      for (int circleY = width / 12; circleY <= midHeight - height / 12; circleY += width / 12){
+        ellipse(circleX, circleY, width / 20, height / 20);
+    }
+  }
   }
   
   // define other methods down here.
